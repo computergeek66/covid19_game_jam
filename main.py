bdf9d9662e4b0293a9c037d4ef4455e8c4fe7560
Trey McGinnis <computergeek66@yahoo.com>
2020-05-29 23:44:07 -0500

    Background sprites and animation

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-import pygame, sys
+import pygame, sys, random
 from pygame.locals import *
 
 
+class Drawable:
+    def __init__(self, sprite, x, y):
+        self.sprite = sprite
+        self.rect = sprite.get_bounding_rect()
+        self.rect.x = x
+        self.rect.y = y
+    
 
 
 FPS=60
@@ -12,6 +19,9 @@ pygame.init()
 block = pygame.image.load("sprites/block.png")
 blockRect = block.get_bounding_rect()
 
+BG_SPRITES = [pygame.image.load("sprites/background/bg1.png"),
+              pygame.image.load("sprites/background/bg2.png")]
+
 DISPLAYSURF = pygame.display.set_mode((400, 600))
 
 DISPLAYHEIGHT = DISPLAYSURF.get_height()
@@ -21,14 +31,23 @@ pygame.display.set_caption("We have Galaga at home!")
 
 BG_COLOR = (136, 0, 21)
 
+bg_tiles = []
 
 def main():
     fpsClock = pygame.time.Clock()
 
+    for i in range(5):
+        bg_tile_sprite = random.choice(BG_SPRITES)
+        bg_tile = Drawable(bg_tile_sprite, random.randrange(DISPLAYWIDTH), random.randrange(DISPLAYHEIGHT))
+        #print(bg_tile_rect.x, ", ", bg_tile_rect.y)
+        bg_tiles.append(bg_tile)
+
+    print(len(bg_tiles))
+
     global blockRect
     
-    blockRect.x = 0.5 * DISPLAYWIDTH
-    blockRect.y = 0.8 * DISPLAYHEIGHT
+    blockRect.x = (int)(0.5 * DISPLAYWIDTH)
+    blockRect.y = (int)(0.8 * DISPLAYHEIGHT)
 
     while True:
         for event in pygame.event.get():
@@ -38,6 +57,14 @@ def main():
 
         ##Fill the frame with background color
         DISPLAYSURF.fill(BG_COLOR)
+
+        for bg_tile in bg_tiles:
+            
+            bg_tile.rect.y += 1
+            if(bg_tile.rect.y > DISPLAYHEIGHT):
+                bg_tile.rect.y = -bg_tile.sprite.get_height()
+            DISPLAYSURF.blit(bg_tile.sprite, bg_tile.rect)
+
 
         keys = pygame.key.get_pressed()
 
