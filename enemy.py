1b2f1ba834878ba4111b5d98b002dbb76593733a
Trey McGinnis <computergeek66@yahoo.com>
2020-05-30 13:21:34 -0500

    Fixed enemy offset

--- enemy.py
+++ enemy.py
@@ -18,25 +18,25 @@ class Enemy:
         if(self.e_type == "rb"):
             self.sprite = pygame.image.load(RB_SPRITE)
             self.speed = 3
-            self.drawable = Drawable(self.sprite, x-self.sprite.get_width()/2, y-self.sprite.get_height()/2)
+            self.drawable = Drawable(self.sprite, x, y)
             self.health = 2
 
         if(self.e_type == "wb"):
             self.sprite = pygame.image.load(WB_SPRITE)
             self.speed = 4
-            self.drawable = Drawable(self.sprite, x-self.sprite.get_width()/2, y-self.sprite.get_height()/2)
+            self.drawable = Drawable(self.sprite, x, y)
             self.health = 5
 
         if(self.e_type == "cb"):
             self.sprite = pygame.image.load(CB_SPRITE)
             self.speed = 2
-            self.drawable = Drawable(self.sprite, x-self.sprite.get_width()/2, y-self.sprite.get_height()/2)
+            self.drawable = Drawable(self.sprite, x, y)
             self.health = 1
 
         if(self.e_type == "vb"):
             self.sprite = pygame.image.load(VB_SPRITES[0])
             self.speed = 2
-            self.drawable = Drawable(self.sprite, x-self.sprite.get_width()/2, y-self.sprite.get_height()/2)
+            self.drawable = Drawable(self.sprite, x, y)
             self.health = 99
 
         
