fbffc9d2a7a3dacaf7bc68322e49c98cad3392ad
Trey McGinnis <computergeek66@yahoo.com>
2020-05-30 17:12:00 -0500

    Drawable Animations

--- drawable.py
+++ drawable.py
@@ -4,3 +4,33 @@ class Drawable:
         self.rect = sprite.get_bounding_rect()
         self.rect.x = x
         self.rect.y = y
+        
+
+    @classmethod
+    def animated_drawable(self, sprite, sprites, x, y):
+        self.sprites = sprites
+        self.rect = sprite.get_bounding_rect()
+        self.sprite_index = 0
+        self.duration = 0
+        self.countdown = 0
+        return self(sprite, x, y)
+
+    def initialize_animation(self, duration, start_time):
+        self.duration = duration
+        self.countdown = start_time
+
+    def animate(self):
+        atEnd = False
+        if(self.countdown>0):
+            self.countdown -=1
+        else:
+            if(self.sprite_index < len(self.sprites)-1):
+                self.sprite_index += 1
+                
+            else:
+                self.sprite_index = 0
+                atEnd = True
+            self.sprite = self.sprites[self.sprite_index]
+            self.countdown = self.duration
+            
+        return atEnd
--- enemy.py
+++ enemy.py
@@ -8,7 +8,9 @@ CB_SPRITE = "sprites/cb.png"
 VB_SPRITES = ["sprites/covid1.png",
               "sprites/covid2.png",
               "sprites/covid3.png",
-              "sprites/covid4.png"]
+              "sprites/covid4.png",
+              "sprites/covid3.png",
+              "sprites/covid2.png"]
 
 class Enemy:
     def __init__(self, e_type, x, y, playWidth):
@@ -36,9 +38,14 @@ class Enemy:
             self.health = 1
 
         if(self.e_type == "vb"):
-            self.sprite = pygame.image.load(VB_SPRITES[0])
-            self.speed = 4*random.uniform(-1.5, 1.5)
-            self.drawable = Drawable(self.sprite, x, y)
+            sprites= []
+            for sprite in VB_SPRITES:
+                sprites.append(pygame.image.load(sprite))
+            self.sprite = sprites[0]
+            self.speed = 4+random.uniform(-1.5, 2)
+            print(self.speed)
+            self.drawable = Drawable.animated_drawable(sprites[0], sprites, x, y)
+            self.drawable.initialize_animation(5,0)
             self.health = 99
 
         
@@ -49,6 +56,9 @@ class Enemy:
             self.velocity *= -1
         self.drawable.rect.x += self.velocity
         self.drawable.rect.y += self.speed
+
+        if(self.e_type == "vb"):
+            self.drawable.animate()
 
     def take_damage(self, damage):
         dead = False
